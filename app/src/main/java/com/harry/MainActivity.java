09f3fee7231cdb34fd214a4062cc0bc728945023
Harpreet S <hsingh@wickr.com>
2015-08-24 16:58:53 -0400

    made easier

--- app/src/main/java/com/harry/MainActivity.java
+++ app/src/main/java/com/harry/MainActivity.java
@@ -4,26 +4,34 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.TextView;
 
 import com.harry.stackLong.Stack;
+
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 /**
  * Created by hsingh on 8/21/15.
  */
 public class MainActivity extends Activity {
+    TextView text;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        text = (TextView) findViewById(R.id.text);
+
     }
 
     public void data(View view) {
-        String input, output;
-        while (true) {
-            Log.d("test", "enter some string");
-//            input = getString()
-        }
+        String input = "";
+        String output = "";
+        input = text.getText().toString();
+        Reverser reverser = new Reverser(input);
+        output = reverser.doRev();
+        text.setText(output);
     }
-
 
 }
